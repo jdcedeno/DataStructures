934c1876a9c7afafabec297d96b6e6076750fce1
JDCedeno <josedcedenocode@gmail.com>
2022-10-19 19:21:49 -0400

    main-06 Set up debugger

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
 "use strict";
 let age = 29;
+if (age < 50)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 let age: number = 29;
+if (age < 50)
+    age += 10;
+console.log(age);
+    
