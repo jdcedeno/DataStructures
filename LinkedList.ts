f6b05f4526733d9670d9ebced0519fad8218e3a0
JDCedeno <josedcedenocode@gmail.com>
2022-10-19 18:52:44 -0400

    main-02 Fix typescript installation

---
+++ LinkedList.ts
@@ -0,0 +1,13 @@
+/**
+ * LOOKUP
+ *      BY VALUE: O(n)
+ *      BY INDEX: O(n)
+ * INSERT
+ *      AT THE END: O(1)
+ *      AT THE BEGINNING: O(1)
+ *      AT THE MIDDLE: O(n)
+ * DELETE
+ *      FROM THE BEGINNING: O(1)
+ *      FROM THE END: O(n)
+ *      FROM THE MIDDLE: O(n)
+ */
